2bb9ce25729af86c8a9bc37013afe48592117f02
suxiang <651029594@qq.com>
2022-01-07 21:50:02 +0800

    dev分支：新增数据库实现接口IProvider，以方便扩展支持更多的数据库

---
+++ DBHelper/DBHelper/Provider/IProvider.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBUtil
+{
+    /// <summary>
+    /// 数据库实现接口
+    /// </summary>
+    interface IProvider
+    {
+        /// <summary>
+        /// 创建 DbConnection
+        /// </summary>
+        DbConnection CreateConnection(string connectionString);
+
+        /// <summary>
+        /// 生成 DbCommand
+        /// </summary>
+        DbCommand GetCommand();
+
+        /// <summary>
+        /// 生成 DbParameter
+        /// </summary>
+        DbParameter GetDbParameter(string name, object vallue);
+
+        /// <summary>
+        /// 带参数的SQL插入和修改语句中，参数前面的符号
+        /// </summary>
+        string GetParameterMark();
+
+        /// <summary>
+        /// 创建获取最大编号SQL
+        /// </summary>
+        string CreateGetMaxIdSql(string key, Type type);
+
+        /// <summary>
+        /// 创建分页SQL
+        /// </summary>
+        string CreatePageSql(string sql, string orderby, int pageSize, int currentPage, int totalRows);
+
+    }
+}
---
+++ DBHelper/DBHelper/Provider/MySQLProvider.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBUtil
+{
+    /// <summary>
+    /// MySQL数据库实现
+    /// </summary>
+    public class MySQLProvider : IProvider
+    {
+        public DbConnection CreateConnection(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreateGetMaxIdSql(string key, Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CreatePageSql(string sql, string orderby, int pageSize, int currentPage, int totalRows)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbCommand GetCommand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbParameter GetDbParameter(string name, object vallue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetParameterMark()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
